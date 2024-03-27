600619b8cbd2a3679ffe264e7fdcf2a4659f9f30
lukelolyan <108691784+lukelolyan@users.noreply.github.com>
2024-03-26 21:05:02 -0400

    Update test.py

--- test.py
+++ test.py
@@ -20,13 +20,14 @@ area = width*length
 
 print("the area of your rectangle is", area)
 
+num5 = 0.25
+num6 = 0.10
+
 num3 = input(" what is the number of your less that 1L bottles?")
 num4 = input("what is the number of your bottle that are more than 1L")
 
-truenum1 = num4 * 25
-truenum1 = num3 * 10
-formatted1 = format(num4, ".2f")
-formatted = format(num3, ".2f")
-last = formatted1 + formatteds
+truenum1 = num4 * num5
+truenum2 = num3 * num6
+last = truenum1+truenum2
 
 print("the amount of cash you will get is $", last)

1c41e55c8bf4352f7af81936e7fe89fa8c053a25
lukelolyan <108691784+lukelolyan@users.noreply.github.com>
2024-03-26 20:32:40 -0400

    test1-5

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print.('hello') 
+import math
+name = "AL"
+number = 20/1
+print("Hi my name is %s  im a code made up of python. I can do math such as, 10+10 " "that equals 30" "Uh Uh, I mean its- its %d" %( name, number))
+
+
+print("hi im Luke and my adress is 156 royal palm drive in Canada Ontario")
+
+name1 = input("whats your name?")
+
+print("great to meet you", name1)
+
+num1 = input(" what is the length of your rectangle?")
+num2 = input("what is the width of your rectangle?")
+
+length = float(num1)
+width = float(num2)
+
+area = width*length
+
+print("the area of your rectangle is", area)
+
+num3 = input(" what is the number of your less that 1L bottles?")
+num4 = input("what is the number of your bottle that are more than 1L")
+
+truenum1 = num4 * 25
+truenum1 = num3 * 10
+formatted1 = format(num4, ".2f")
+formatted = format(num3, ".2f")
+last = formatted1 + formatteds
+
+print("the amount of cash you will get is $", last)

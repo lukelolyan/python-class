1eb111a8e681a4e30acf7514c9424dfdcd18cf10
lukelolyan <108691784+lukelolyan@users.noreply.github.com>
2024-03-26 21:35:30 -0400

    exercises

---
+++ exercise1.py
@@ -0,0 +1,3 @@
+name1 = input("whats your name?")
+
+print("great to meet you", name1)
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+num1 = input(" what is the length of your rectangle?")
+num2 = input("what is the width of your rectangle?")
+
+length = float(num1)
+width = float(num2)
+
+area = width*length
+
+print("the area of your rectangle is", area)

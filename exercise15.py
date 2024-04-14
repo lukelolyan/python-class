a067102f286952ca068156729d58e23dc0b6d6fc
lukelolyan <108691784+lukelolyan@users.noreply.github.com>
2024-04-14 09:53:15 -0400

    Create exercise15.py

---
+++ exercise15.py
@@ -0,0 +1,9 @@
+feet = float(input("Enter the distance in feet: "))
+
+inches = feet * 12
+yards = feet / 3
+miles = feet / 5280
+
+print("Equivalent distance in inches:", inches)
+print("Equivalent distance in yards:", yards)
+print("Equivalent distance in miles:", miles)
